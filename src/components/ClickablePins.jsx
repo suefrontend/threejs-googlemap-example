2fe669bff0b014a36ff10e6dd69461ea2576b6e8
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-31 23:41:03 -0700

    Update

--- src/components/ClickablePins.jsx
+++ src/components/ClickablePins.jsx
@@ -1,8 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as THREE from "three";
 import { ThreeJSOverlayView } from "@googlemaps/three";
+import { getGeocode, getLatLng } from "use-places-autocomplete";
 
 function ClickablePins(props) {
+  const [places, setPlaces] = useState([]);
+
+  const hotels = [
+    { name: "Hyatt Regency Vancouver" },
+    { name: "Fairmont Hotel Vancouver" },
+    { name: "Rosewood Hotel Georgia" },
+    { name: "Metropolitan Hotel Vancouver" },
+    { name: "Days Inn by Wyndham Vancouver Downtown" },
+    { name: "St Regis Hotel" },
+    { name: "EXchange Hotel Vancouver - An Executive Hotel" },
+    { name: "Shangri-La Vancouver" },
+    { name: "Vancouver Marriott Pinnacle Downtown Hotel" },
+    { name: "Delta Hotels by Marriott Vancouver Downtown Suites" },
+    { name: "Loden Hotel" },
+    { name: "Wedgewood Hotel & Spa" },
+  ];
+
+  async function fetchDirections(name) {
+    const locationResult = await getGeocode({ address: name });
+    const locationLatLng = await getLatLng(locationResult[0]);
+
+    // console.log("locationLatLng", locationLatLng);
+    return locationLatLng;
+  }
+
   useEffect(() => {
     props.map.setCenter(props.mapOptions.center);
     const overlay = new ThreeJSOverlayView({
@@ -16,6 +42,10 @@ function ClickablePins(props) {
     overlay.setMap(props.map);
 
     const scene = overlay.scene;
+
+    for (const hotel of hotels) {
+      fetchDirections(hotel.name);
+    }
 
     // はじめ
     /// まずはピンを表示させる
